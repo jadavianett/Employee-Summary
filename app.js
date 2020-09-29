99406574e249f6786822ed55beaff24c8a5d168e
Jada Arnett <jada.arnett@gmail.com>
2020-09-29 14:27:04 -0400

    added start question function which will prompt the user with the first question to start the application

--- app.js
+++ app.js
@@ -12,99 +12,98 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 const team = [];
-console.log("Follow the prompts to build your team!");
+
 const questionPrompt = [
-    { 
-        type: "confirm",
-        message: "Would you like to enter a team member?",
-        name: "initQuestion",
-    }
+  {
+    type: "confirm",
+    message: "Would you like to build your team?",
+    name: "initQuestion",
+  },
 ];
 const newMember = [
-    {
-       type: "confirm",
-       message: "Would you like to add another team member?",
-       name: "newMember" 
-    }
+  {
+    type: "confirm",
+    message: "Would you like to add another team member?",
+    name: "newMember",
+  },
 ];
 
 const typeOfTeamMember = [
-{ 
+  {
     type: "list",
     message: "Which member of your team would you like to add?",
     choices: ["Engineer", "Intern", "Manager"],
-    name: "typeofTeamMember"
-}
+    name: "memberType",
+  },
 ];
 
-const engineerQuestions = [
-    {
-        type: "input",
-        message: "What is your engineer's name?",
-        name: "name"
-      },
-      {
-        type: "input",
-        message: "What is your engineer's ID?",
-        name: "id"
-      },
-      {
-        type: "input",
-        message: "What is your engineer's e-mail?",
-        name: "email"
-      },
-      {
-        type: "input",
-        message: "What is your engineer's GitHub username?",
-        name: "github"
-      },
+const engineerPrompts = [
+  {
+    type: "input",
+    message: "What is your engineer's name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is your engineer's ID?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is your engineer's e-mail?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is your engineer's GitHub username?",
+    name: "github",
+  },
 ];
 
-const internQuestions = [
-    {
-        type: "input",
-        message: "What is your intern's name?",
-        name: "name"
-      },
-      {
-        type: "input",
-        message: "What is your intern's ID?",
-        name: "id"
-      },
-      {
-        type: "input",
-        message: "What is your intern's e-mail?",
-        name: "email"
-      },
-      {
-        type: "input",
-        message: "What is your intern's school?",
-        name: "school"
-      },
+const internPrompts = [
+  {
+    type: "input",
+    message: "What is your intern's name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is your intern's ID?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is your intern's e-mail?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is your intern's school?",
+    name: "school",
+  },
 ];
 
-const managerQuestions = [ 
-    {
-        type: "input",
-        message: "What is your manager's name?",
-        name: "name"
-      },
-      {
-        type: "input",
-        message: "What is your manager's ID?",
-        name: "id"
-      },
-      {
-        type: "input",
-        message: "What is your manager's e-mail?",
-        name: "email"
-      },
-      {
-        type: "input",
-        message: "What is your manager's office number?",
-        name: "officeNumber"
-      },
-
+const managerPrompts = [
+  {
+    type: "input",
+    message: "What is your manager's name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is your manager's ID?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is your manager's e-mail?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is your manager's office number?",
+    name: "officeNumber",
+  },
 ];
 
 // TODO, ASK QUESTION BASED ON SPECIFIC USER INPUT, IE, IF THEY SELECT ADD AN INTERN, ASK THEM INTERN QUESTIONS!
@@ -115,6 +114,37 @@ const managerQuestions = [
 // generate and return a block of HTML including templated divs for each employee!
 
 
+// starts application
+function startQuestions() {
+  inquirer.prompt(questionPrompt).then(function (data) {
+      if (data.questionPrompt === true) {
+        console.log("will start questions!");
+        // memberType();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+// adds new team members 
+// function addToTeam () {
+//     inquirer.prompt(typeOfTeamMember).then(function (data) {
+//         if (data.newMember === true) {
+// memberType();
+//         } else {
+//             end();
+//         }
+//     }
+//     ).catch((err) => {
+//         console.log(err);
+//     }
+//     )
+
+// }
+
+//chooses type of team member
+// function memberType ()
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
@@ -126,8 +156,17 @@ const managerQuestions = [
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
 
+function init() {
+    console.log("Follow the prompts to build your team!");
+    startQuestions();
+}
+
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+
+//FUNCTION CALLS 
+init();
